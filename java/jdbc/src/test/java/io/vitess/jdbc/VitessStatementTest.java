cc2ef0be5f61222aa370bde1fdac1c074eb713ef
Bryan Beaudreault <bbeaudreault@hubspot.com>
2017-08-14 12:34:29 -0400

    fix import

--- java/jdbc/src/test/java/io/vitess/jdbc/VitessStatementTest.java
+++ java/jdbc/src/test/java/io/vitess/jdbc/VitessStatementTest.java
@@ -28,6 +28,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
