6382e60483c6ffc8a3df6ed1ec03af74803a0fec
Megan K <5599341+Xcelled@users.noreply.github.com>
2020-05-29 11:18:23 -0400

    Explicitly set NioSocketChannel (#39)
    * Explicitly set NioSocketChannel
    
    * Fix import
    
    * Derp wrong method

--- java/grpc-client/src/main/java/io/vitess/client/grpc/netty/DefaultChannelBuilderProvider.java
+++ java/grpc-client/src/main/java/io/vitess/client/grpc/netty/DefaultChannelBuilderProvider.java
@@ -3,6 +3,7 @@ package io.vitess.client.grpc.netty;
 import io.grpc.netty.NettyChannelBuilder;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.concurrent.DefaultThreadFactory;
 import io.vitess.client.grpc.RetryingInterceptor;
 import io.vitess.client.grpc.RetryingInterceptorConfig;
@@ -22,6 +23,7 @@ public class DefaultChannelBuilderProvider implements NettyChannelBuilderProvide
   @Override
   public NettyChannelBuilder getChannelBuilder(String target) {
     return NettyChannelBuilder.forTarget(target)
+        .channelType(NioSocketChannel.class)
         .eventLoopGroup(ELG)
         .maxInboundMessageSize(16777216)
         .intercept(new RetryingInterceptor(config));
